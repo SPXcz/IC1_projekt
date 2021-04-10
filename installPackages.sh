e4545511ef034d3c79956b75c7ef1dada281afc3
Roman Klampar <47435706+Romx98@users.noreply.github.com>
2021-04-11 00:51:11 +0200

    update installPackages.sh
    doplnenie instalacie dockeru, docker-compose, curl, opennssh

--- installPackages.sh
+++ installPackages.sh
@@ -6,6 +6,26 @@
 # Instalovane balicky:
 #	> gcc 
 #	> make
+# > curl
+# > docker + docker-compose
 
+# packages
 sudo apt-get --assume-yes install gcc
 sudo apt-get --assume-yes install make
+sudo apt-get --assume-yes install curl
+
+# install openssh
+sudo apt-get --assume-yes install openssh-server
+
+# install DOCKER
+sudo apt --assume-yes install docker.io
+sudo systemctl start docker
+sudo systemctl enable docker
+
+# install DOCKER-COMPOSE
+sudo curl -L https://github.com/docker/compose/releases/download/1.24.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+
+echo "Verzia docker: `docker --version`"
+echo "Verzia docker-compose: `docker–compose –-version`"
